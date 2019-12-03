9d3ab0d51afff4b4b0bb2d4f0e1b20ff45601591
frey <freyprogrammer@gmail.com>
2019-12-03 12:58:05 +0800

    update data binding

--- javascript-snippets/data-binding/data-binding.js
+++ javascript-snippets/data-binding/data-binding.js
@@ -1,21 +1,3 @@
-// let c1 = {
-//     x: 5,
-//     y: 10
-// }
-
-// let c2 = {
-//     x: 35,
-//     y: 769
-// }
-
-// function printCoordinates() {
-//     console.log(this.name+', '+this.y);
-// }
-
-// let c1_Function = printCoordinates.bind(c1);
-
-// c1_Function();
-
 let details1 = {
     name: document.getElementById("name"),
     address: document.getElementById("address"),
