741843ca7770ebfaa229fd2a3037127f1704ed11
frey <freyprogrammer@gmail.com>
2019-12-05 16:22:37 +0800

    update javascript classes

--- javascript-es6-classes/class.js
+++ javascript-es6-classes/class.js
@@ -1,9 +1,33 @@
 class Bubble {
-    constructor(){
+    constructor() {
         this.x = 200;
         this.y = 150
     }
+
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+        
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        eclipse(this.x, this.y, 24, 24);
+    }
 }
 
-let bubble = new Bubble;
-console.log(bubble.x, bubble.y);
+let bubble1 = new Bubble;
+let bubble2 = new Bubble;
+
+function draw() {
+    background(0);
+    bubble1.move();
+    bubble1.show();
+    bubble2.move();
+    bubble2.show();
+    
+}
+
+draw();
